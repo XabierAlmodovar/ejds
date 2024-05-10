7a780f388238f5ac2e84703ac6073ac89985acf9
Xabier Almodovar <xabier.almodovar@alumni.mondragon.edu>
2024-05-10 09:39:04 +0200

    Segundo commit, de media de precio

--- Commmit.R
+++ Commmit.R
@@ -6,6 +6,7 @@ df<-read_xlsx("online_retail_II.xlsx")
 
 str(df)
 df$`Customer ID`<-as.character(df$`Customer ID`)
+df$Invoice<-
 
 library(naniar)
 miss_var_summary(df) # 20% en descripcion pero tampoco es tan importante
@@ -14,3 +15,13 @@ miss_case_summary(df) # como mucho un 25%, siendo descripcion 1 de los 2 misses 
 str(df)
 length(unique(df$Invoice)) # 28816 factutas unicas
 
+library(dplyr)
+df$Price<-as.numeric(df$Price)
+
+unique(datos$Description)
+library(dplyr)
+df_media<-df %>%
+  group_by(Invoice)%>%
+  summarise(precio_medio=mean(Price))
+
+df_media
